3201882e6de964c3bd14b8b92d381e10a1381863
PC} <PC}@DESKTOP-FF1OI0F>
2025-03-25 11:55:39 +0100

    Primera Version

---
+++ DatosJorgeLuisAymaSaavedra/src/datosjorgeluisaymasaavedra/Principal.java
@@ -0,0 +1,11 @@
+package datosjorgeluisaymasaavedra;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Jorge Luis Ayma Saavedra");
+	}
+
+}
